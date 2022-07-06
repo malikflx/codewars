2ea6855ac2e2dedd347652e720fb35c3e9735eab
Malik Felix <malikfelix50@gmail.com>
2022-07-05 23:09:40 -0400

    adding list-filtering

---
+++ 7kyu/list-filtering.js
@@ -0,0 +1,19 @@
+// Task
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+// Solution
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  // Step 1
+  let nums = l.filter(Number.isFinite)
+  
+  return nums
+}
+
+// Step 1: Filter array for type of Number. Use isFinite to get around falsy 0 value.
