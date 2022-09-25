6ca07a9a73465ceb4b99088277be434509d56828
Malik Felix <malikfelix50@gmail.com>
2022-09-25 19:24:19 -0400

    add reverse-list-order

---
+++ 8kyu/reverse-list-order.js
@@ -0,0 +1,7 @@
+// Task
+// In this kata you will create a function that takes in a list and returns a list with the reverse order.
+
+// Solution
+function reverseList(list) {
+  return list.reverse();
+}
