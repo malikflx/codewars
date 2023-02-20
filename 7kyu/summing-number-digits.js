3d11b5d94cd375361ddf3eeadb1228599e973743
Malik <malikfelix50@gmail.com>
2023-02-20 14:15:56 -0600

    update solution for summing-number-digits

--- 7kyu/summing-number-digits.js
+++ 7kyu/summing-number-digits.js
@@ -9,19 +9,21 @@
 
 // Solution
 function sumDigits(number) {
-  // Step 1
-  const decimalFormat = (Math.abs(number) / 10).toFixed(1);
-  // Step 2
-  const decimalSum = decimalFormat.replace(".", "+");
-  // Step 3
-  return eval(decimalSum);
+  number = Math.abs(number)
+    .toString()
+    .split("")
+    .map((i) => Number(i))
+    .reduce((acc, curr) => acc + curr, 0);
+  return number;
 }
 
-// Step 1: Convert number to decimal format using toFixed(). I'd have to use Math.abs() to get the absolute value of the number. This returns a string.
-// Step 2: Use replace() to remove the decimals and replace it with "+"; Now "1.0" become "1+0".
-// Step 3: Evaluate the number sentence using the eval() function which takes the string a adds it as an expression with integers.
+// Step 1: Convert the absolute value of a number to a String using Math.abs() and toString().
+// Step 2: Turn the string into an array of strings using toString().
+// Step 3: Use map() to turn each string in an array to an integer (number) using Number().
+// Step 4: Use reduce() to get the summation of all numbers in the array.
 
 // Test Cases
 console.log(sumDigits(10));
 console.log(sumDigits(49));
 console.log(sumDigits(-43));
+console.log(sumDigits(6789988529));
