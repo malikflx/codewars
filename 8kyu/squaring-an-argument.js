72a7a02d2c77b3be3d90e34bb1e5a2768a609001
Malik <malikfelix50@gmail.com>
2023-02-27 07:33:08 -0600

    add squareing-an-argument

---
+++ 8kyu/squaring-an-argument.js
@@ -0,0 +1,15 @@
+// Task
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// Solution:
+const square = (num) => {
+  // Step 1
+  return Math.pow(num, 2);
+};
+
+// Step 1: Use Math.pow() on the argument to return the squared value of it.
+
+// Test Cases
+console.log(square(4));
+console.log(square(12));
+console.log(square(9));
